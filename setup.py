d6d6cd811ab5bb0bc127f49b096d4d3b2515b097
Joost Molenaar <j.j.molenaar@gmail.com>
2012-11-12 20:22:34 +0100

    Extracted sh.py with useful shell-like functions

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import distutils.core
+distutils.core.setup(
+    author="Joost Molenaar",
+    author_email="j.j.molenaar@gmail.com",
+    name="jjm.sh",
+    packages=["jjm.sh"],
+    url="http://github.com/JoostMolenaar/jjm.sh",
+    version="0.1.0"
+)
