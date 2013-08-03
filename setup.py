96221bee11c7d30c9d7f0bfc49c3d95b88c15fef
Joost Molenaar <j.j.molenaar@gmail.com>
2013-08-03 10:39:44 +0200

    Rename jjm.sh to sh

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ import distutils.core
 distutils.core.setup(
     author="Joost Molenaar",
     author_email="j.j.molenaar@gmail.com",
-    name="jjm.sh",
-    packages=["jjm.sh"],
-    url="http://github.com/JoostMolenaar/jjm.sh",
+    name="sh",
+    packages=["sh"],
     version="0.1.0"
 )
