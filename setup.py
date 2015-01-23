7cad079d52b12e692bffa408731fb4c43acc8f8f
Joost Molenaar <j.j.molenaar@gmail.com>
2015-01-23 23:05:46 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-import distutils.core
-distutils.core.setup(
-    author="Joost Molenaar",
-    author_email="j.j.molenaar@gmail.com",
+#!/usr/bin/env python2
+import setuptools
+setuptools.setup(
     name="sh",
+    version="0.1.0",
     packages=["sh"],
-    version="0.1.0"
-)
+    author="Joost Molenaar",
+    author_email="j.j.molenaar@gmail.com")
